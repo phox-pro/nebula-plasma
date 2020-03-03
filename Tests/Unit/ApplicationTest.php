86c76b3709d7a56516d2ae18bab41c328726c890
Maksim Lifintcev <hullabaloo@phox.pro>
2020-03-03 15:56:47 +0300

    PLASMA-1 Default plasma application logic

--- Tests/Unit/ApplicationTest.php
+++ Tests/Unit/ApplicationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Phox\Nebula\Plasma\TestCase;
 use Phox\Nebula\Atom\Implementation\Application;
 use Phox\Nebula\Plasma\Implementation\Application as PlasmaApplication;
+use Phox\Nebula\Plasma\Notion\Abstracts\Star;
 
 class ApplicationTest extends TestCase 
 {
@@ -14,5 +15,34 @@ class ApplicationTest extends TestCase
     public function injectionTest()
     {
         $this->assertInstanceOf(PlasmaApplication::class, get(Application::class));
+    }
+
+    /**
+     * @test
+     */
+    public function starSetTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $this->assertNull($app->getStar());
+        $mock = $this->createMock(Star::class);
+        $app->setStar($mock);
+        $this->assertInstanceOf(Star::class, $app->getStar());
+        $this->assertSame($mock, $app->getStar());
+    }
+
+    /**
+     * @test
+     */
+    public function actionSetTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $mock = $this->createMock(Star::class);
+        $this->assertNull($app->getAction());
+        $app->setAction('action');
+        $this->assertEquals('action', $app->getAction());
+        $app->setAction();
+        $this->assertNull($app->getAction());
     }
 }
--- src/Implementation/Application.php
+++ src/Implementation/Application.php
@@ -4,9 +4,14 @@ namespace Phox\Nebula\Plasma\Implementation;
 
 use Phox\Nebula\Atom\Implementation\Application as AtomApplication;
 use Phox\Nebula\Atom\Implementation\Exceptions\StateExistsException;
+use Phox\Nebula\Plasma\Notion\Abstracts\Star;
 
 class Application extends AtomApplication 
 {
+    protected ?Star $star = null;
+
+    protected ?string $action = null;
+
 	public function __construct()
 	{
         try {
@@ -14,5 +19,25 @@ class Application extends AtomApplication
         } catch (StateExistsException $e) {
             //
         }
-	}
+    }
+    
+    public function setStar(Star $star)
+    {
+        $this->star = $star;
+    }
+
+    public function getStar() : ?Star
+    {
+        return $this->star;
+    }
+
+    public function setAction(?string $action = null)
+    {
+        $this->action = $action;
+    }
+
+    public function getAction() : ?string
+    {
+        return $this->action;
+    }
 }
