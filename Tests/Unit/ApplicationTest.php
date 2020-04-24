39a9df6571d2b22cfba0e7b5e108933621d6d0fa
Maksim Lifintcev <hullabaloooooooo@gmail.com>
2020-04-24 23:45:00 +0300

    StarEvent. Output

--- Tests/Unit/ApplicationTest.php
+++ Tests/Unit/ApplicationTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use Phox\Nebula\Plasma\TestCase;
 use Phox\Nebula\Atom\Implementation\Application;
 use Phox\Nebula\Plasma\Implementation\Application as PlasmaApplication;
+use Phox\Nebula\Plasma\Implementation\Events\StarEvent;
+use Phox\Nebula\Plasma\Implementation\Exceptions\StarNotSetted;
 use Phox\Nebula\Plasma\Notion\Abstracts\Star;
 
 class ApplicationTest extends TestCase 
@@ -25,6 +27,7 @@ class ApplicationTest extends TestCase
         /** @var PlasmaApplication $app */
         $app = get(PlasmaApplication::class);
         $this->assertNull($app->getStar());
+        /** @var Star $mock */
         $mock = $this->createMock(Star::class);
         $app->setStar($mock);
         $this->assertInstanceOf(Star::class, $app->getStar());
@@ -44,5 +47,108 @@ class ApplicationTest extends TestCase
         $this->assertEquals('action', $app->getAction());
         $app->setAction();
         $this->assertNull($app->getAction());
+    }
+
+    /**
+     * @test
+     */
+    public function starNotSettedTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $this->expectException(StarNotSetted::class);
+        $app->run();
+    }
+
+    /**
+     * @test
+     */
+    public function echoFromStarTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $app->setStar(new class extends Star {
+            public function __invoke() {
+                return "Hello Test";
+            }
+        });
+        $this->expectOutputString("Hello Test");
+        $app->run();
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function getOutputTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $this->assertNull($app->getOutput());
+        $app->setStar(new class extends Star {
+            public function __invoke() {
+                return "Output";
+            }
+        });
+        $this->expectOutputString("Output");
+        $app->run();
+        $this->assertEquals("Output", $app->getOutput());
+    }
+
+    /**
+     * @test
+     */
+    public function outputFromActionTest()
+    {
+        $app = get(PlasmaApplication::class);
+        $app->setStar(new class extends Star {
+            public function someAction() {
+                return "Work!";
+            }
+        });
+        $app->setAction('someAction');
+        $this->expectOutputString("Work!");
+        $app->run();
+    }
+
+    /**
+     * @test
+     */
+    public function nullabaleStarResultTest()
+    {
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $app->setStar(new class extends Star {
+            public function __invoke() {
+                // return void
+            }
+        });
+        $app->run();
+        $this->assertNull($app->getOutput());
+    }
+
+    /**
+     * @test
+     */
+    public function starEventTest()
+    {
+        $this->assertEmpty(StarEvent::getListeners());
+        $star = new class extends Star {
+            public bool $value = false;
+
+            public function __invoke() {
+                $this->value = true;
+            }
+        };
+        /** @var PlasmaApplication $app */
+        $app = get(PlasmaApplication::class);
+        $app->setStar($star);
+        $this->assertFalse($app->getStar()->value);
+        StarEvent::listen(function (Star $getStar) use ($star) {
+            $this->assertSame($getStar, $star);
+            $this->assertTrue($getStar->value);
+        });
+        $app->run();
     }
 }
--- src/Implementation/Application.php
+++ src/Implementation/Application.php
@@ -4,8 +4,8 @@ namespace Phox\Nebula\Plasma\Implementation;
 
 use Phox\Nebula\Atom\Implementation\Application as AtomApplication;
 use Phox\Nebula\Atom\Implementation\Basics\Collection;
-use Phox\Nebula\Atom\Implementation\Exceptions\StateExistsException;
-use Phox\Nebula\Atom\Notion\Interfaces\IStateContainer;
+use Phox\Nebula\Plasma\Implementation\Events\StarEvent;
+use Phox\Nebula\Plasma\Implementation\Exceptions\StarNotSetted;
 use Phox\Nebula\Plasma\Notion\Abstracts\Star;
 
 class Application extends AtomApplication 
@@ -13,6 +13,8 @@ class Application extends AtomApplication
     protected ?Star $star = null;
 
     protected ?string $action = null;
+
+    protected ?string $output = null;
 
 	public function __construct(Collection $providers)
 	{
@@ -37,5 +39,20 @@ class Application extends AtomApplication
     public function getAction() : ?string
     {
         return $this->action;
+    }
+
+    public function getOutput() : ?string
+    {
+        return $this->output;
+    }
+
+    protected function enrichment()
+    {
+        parent::enrichment();
+        isset($this->star)
+            ? $this->output = call(is_null($this->action) ? $this->star : [$this->star, $this->action])
+            : error(StarNotSetted::class);
+        StarEvent::notify($this->star);
+        echo $this->output;
     }
 }
